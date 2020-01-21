448df17f991c6a77d97dae8f12726ab28431c6f0
garywangop <wg900116@ges-MacBook-Pro.local>
2020-01-21 01:24:41 -0500

    Stage 1

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Topbar } from './Topbar';
+import { Register } from './Register';
+import '../styles/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Topbar />
+            <Register />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Topbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+import '../styles/Topbar.css';
+
+export function Topbar() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Around</h1>
+        </header>
+    );
+}
