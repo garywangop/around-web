5b814eead12b8a86fdc07e51d600ae209a50a4d8
garywangop <garywangop@gmail.com>
2020-01-29 01:44:15 -0500

    new laptop test

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,9 @@
-export const API_ROOT = 'https://around-dot-around-179500.appspot.com/api/v1';
+export const API_ROOT = 'https://around-179500.appspot.com';
 export const TOKEN_KEY = 'TOKEN';
+export const POSITION_KEY = 'POSITION';
+export const AUTH_HEADER = 'Bearer';
+export const GEOLOCATION_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge: 3600000,
+    timeout: 27000,
+};
